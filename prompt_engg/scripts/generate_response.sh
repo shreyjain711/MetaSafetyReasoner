647ba58bea7dd5f729f96db3b6a933ab05f95772
Yijie Ding <jamesdin@cs.cmu.edu>
2025-10-10 22:19:07 -0700

    added results on multi_turn_consolidated_one dataset

--- prompt_engg/scripts/generate_response.sh
+++ prompt_engg/scripts/generate_response.sh
@@ -4,20 +4,27 @@
 
 set -euo pipefail
 
-# meta-llama/Llama-3.2-3B-Instruct
-# Qwen/Qwen3-30B-A3B-Instruct-2507-FP8
-# open-thoughts/OpenThinker3-7B
-# deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
+# Models:
+    # meta-llama/Llama-3.2-3B-Instruct
+    # Qwen/Qwen3-30B-A3B-Instruct-2507-FP8
+    # open-thoughts/OpenThinker3-7B
+    # deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
 
-MODEL_NAME="open-thoughts/OpenThinker3-7B"
+# Datasets:
+    # val_data/multi_turn_subset_224.json
+    # val_data/MSR_BeaverTails_4x56_subset.json
+
+MODEL_NAME="Qwen/Qwen3-30B-A3B-Instruct-2507-FP8"
 CLIENT="vllm"
-DATA_FILE="val_data/MSR_BeaverTails_4x56_subset.json"
+DATA_FILE="val_data/multi_turn_subset_224.json"
 BATCH_SIZE=250
 SCORE_PROMPT=True
+PROMPT_FEILD="user_input"
 
 python code/main.py \
     --model_name $MODEL_NAME \
     --client=$CLIENT \
     --data_file=$DATA_FILE \
     --batch_size=$BATCH_SIZE \
-    --score_prompt=$SCORE_PROMPT
+    --score_prompt=$SCORE_PROMPT \
+    --prompt_feild=$PROMPT_FEILD
