8a9feb0480fdceb60ba0474cb74f37667f57e2c6
shreyjain711 <shrey.jain711@gmail.com>
2025-10-26 08:06:25 -0400

    added code for continuing inference

--- prompt_engg/code/data_generator.py
+++ prompt_engg/code/data_generator.py
@@ -20,21 +20,24 @@ def get_promt_from_file(prompt_file_path):
             return (prompt_data.get('prompts').get('system').strip(), prompt_data.get('prompts').get('user').strip())
 
 
-def dataset_reader(file_path):
+def dataset_reader(file_path, skip_lines):
     if file_path.startswith('hf://'):
         dataset = load_dataset(file_path[5:])
-        for item in dataset['train']:
+        for item in dataset['train'].select(range(skip_lines, len(dataset['train']))):
             yield item
     else:
         with open(file_path, 'r') as f:
             for line in f:
+                if skip_lines > 0:
+                    skip_lines -= 1
+                    continue
                 yield json.loads(line)
 
 
-def data_generator(file_path, batch_size, prompt_file_path=None):
+def data_generator(file_path, batch_size, prompt_file_path=None, skip_lines=0):
     user_prompt, system_prompt = get_promt_from_file(prompt_file_path)
     batch = []
-    for data in dataset_reader(file_path):
+    for data in dataset_reader(file_path, skip_lines):
         batch.append((data, create_message(data, user_prompt, system_prompt)))
         if len(batch) == batch_size:
             yield batch
